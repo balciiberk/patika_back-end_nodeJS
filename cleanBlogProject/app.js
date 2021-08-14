5bad0792e6df68841364b54e65445dfe23589520
berk <balciiberk@gmail.com>
2021-08-14 23:18:03 +0300

    HW2 | added static files & added ejs

--- cleanBlogProject/app.js
+++ cleanBlogProject/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const port = 3000;
 const host = 'localhost';
@@ -6,9 +7,24 @@ const blog = { id: 1, title: "Blog title", description: "Blog description" };
 
 const app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req,res) => {
-	res.send(blog)
+	//res.send(blog)
+	res.render('index');
 });
+
+app.get('/about', (req,res) => {
+	//res.send(blog)
+	res.render('about');
+});
+
+app.get('/add', (req,res) => {
+	//res.send(blog)
+	res.render('add_post');
+});
+
 
 app.listen(port, host, () =>{
 	console.log(`server started at ${host}:${port}`)
